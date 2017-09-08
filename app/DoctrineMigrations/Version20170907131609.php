448823265f1eea0812a8d2bc6a5c98807416d4c2
JordanBeziau <jordanbeziau@gmail.com>
2017-09-08 09:57:26 +0200

    Prepare index & admin views + install fixtures

---
+++ app/DoctrineMigrations/Version20170907131609.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170907131609 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE flight (id INT AUTO_INCREMENT NOT NULL, flight_code VARCHAR(5) NOT NULL, departure VARCHAR(255) NOT NULL, arrival VARCHAR(255) NOT NULL, time TIME NOT NULL, seat INT NOT NULL, price INT NOT NULL, information TINYINT(1) NOT NULL, alert TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_C257E60EF62B6DFF (flight_code), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE flight');
+    }
+}
--- src/AirwaysBundle/Controller/AirwaysController.php
+++ src/AirwaysBundle/Controller/AirwaysController.php
@@ -5,13 +5,20 @@ namespace AirwaysBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class AirwaysController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
         return $this->render('AirwaysBundle:Default:index.html.twig');
     }
+
+  /**
+   * @Route("/admin", name="admin")
+   */
+  public function adminAction() {
+    return $this->render("AirwaysBundle:default:admin.html.twig");
+  }
 }
